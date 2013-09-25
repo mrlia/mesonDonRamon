209cebdc0d3744d10e49d3cf8427f49d2e9302c8
Rosa del Rio <charmed.lia@gmail.com>
2013-09-25 22:00:51 +0100

    Updated for heroku to get the static files

--- mesonDonRamon/settings.py
+++ mesonDonRamon/settings.py
@@ -1,10 +1,18 @@
 # Django settings for mesonDonRamon project.
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Rosa del Rio', 'charmed.lia@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -61,11 +69,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
