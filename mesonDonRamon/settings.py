b35d996d4bad84e1fb1bd47046f7fcbc2bb49d6a
Rosa del Rio <charmed.lia@gmail.com>
2013-09-25 22:21:56 +0100

    Minor change

--- mesonDonRamon/settings.py
+++ mesonDonRamon/settings.py
@@ -17,13 +17,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DB_PATH = (
-    os.path.join(PROJECT_PATH, '../mesonDB.db'),
-)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': DB_PATH,  # Or path to database file if using sqlite3.
+        'NAME': 'mesonDB.db',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
